e55f14a751554ebe2cf33671e7e4e7033dfd1d8a
danielgstephenson <dgstephenson.econ@gmail.com>
2026-04-12 22:35:35 -0400

    life objective

--- objective.py
+++ objective.py
@@ -11,21 +11,16 @@ def get_life(state: Tensor)->Tensor:
     blade_distance = torch.norm(blade_vector,p=2,dim=1, keepdim=True)
     return (blade_distance > 15).to(physics_dtype)
 
-def get_quality(state: Tensor)->Tensor:
-    return 100*get_life(state)
-
 discount = 0.9
 other_noise = 0.3
 other_passive = 0
 def get_action_values(value_model: ValueModel, state: Tensor, outcomes: Tensor, horizon: int):
     with torch.no_grad():
         life0 = get_life(state).repeat_interleave(81, dim=0).reshape(-1,9,9)
-        life1 = get_life(outcomes).reshape(-1,9,9)
-        life1 = life0 * life1
+        life1 = life0 * get_life(outcomes).reshape(-1,9,9)
         reward = 100*(life1 - life0)
         if horizon > 0:
-            next_values = value_model(outcomes).reshape(-1,9,9)
-            next_values = torch.where(life0 > 0, next_values, 0)
+            next_values = life0 * value_model(outcomes).reshape(-1,9,9)
             value = reward + discount*next_values
         else:
             value = reward
