1c87ccdc353e45936abe8c5341678c6b8d6d8c52
danielgstephenson <dgstephenson.econ@gmail.com>
2026-05-19 08:41:51 -0400

    reward

--- attack/generator.py
+++ attack/generator.py
@@ -89,14 +89,15 @@ class DataGenerator:
 
     def generate(self, horizon: int)->tuple[Tensor,...]:
         self.value_model.eval()
-        p = 0.01 # Discount Rate
+        p = 0.001 # Discount Rate
+        c = 0.1 # Caution
         with torch.no_grad():
             self.reset()
             self.update(horizon)
             state = self.state.clone()
             life0 = self.life0.clone()
             life1 = self.life1.clone()
-            reward = 0.5 * life0 + 0.5 * (1 - life1)
+            reward = c * life0 + (1-c) * life0 * (1 - life1)
             value_target = reward * p
             for t in range(self.step_count):
                 self.simulation.step()
@@ -104,10 +105,10 @@ class DataGenerator:
                 life0 *= self.life0
                 life1 *= self.life1
                 end_prob = p * (1-p) ** (t+1)    
-                reward = 0.5 * life0 + 0.5 * (1 - life1)
+                reward = c * life0 + (1-c) * life0 * (1 - life1)
                 value_target += reward * end_prob
             outcome = self.state.clone()
-            reward = 0.5 * life0 + 0.5 * (1 - life1)
+            reward = c * life0 + (1-c) * life0 * (1 - life1)
             continuation_prob = (1-p) ** (self.step_count+1)
             continuation_value = reward if horizon == 0 else F.sigmoid(self.value_model(outcome))
             value_target += continuation_value * continuation_prob
